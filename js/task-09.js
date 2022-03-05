f08b259f21c1c9e6e5b491bacf1adf66e08d5902
Yurii <yuramasha13@gmail.com>
2022-03-05 18:02:22 +0200

    Adds task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
+const refs = {
+  btnEl: document.querySelector('.change-color'),
+  spanColorEl: document.querySelector('.color'),
+}
+
+refs.btnEl.addEventListener('click', () => {
+  refs.spanColorEl.style.color = getRandomHexColor();
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
